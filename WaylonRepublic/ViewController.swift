16f1ffd09c7000f5c0270261fbd852e6771a5a08
bwayne500 <bwayne500elite@gmail.com>
2022-03-13 15:26:03 +0800

    Initial Commit

---
+++ WaylonRepublic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WaylonRepublic
+//
+//  Created by bwayne500 on 2022/3/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
